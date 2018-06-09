cd4f1ade00197dd6fa1a81da8f2c26a0ba6a39ca
Juno Lee <lejuno80@gmail.com>
2018-06-09 12:13:45 -0500

    added part1

--- javascripts/weather.js
+++ javascripts/weather.js
@@ -31,9 +31,34 @@ const showResults = () => {
     .catch((err) => {
       console.error('search error', err);
     });
+
+};
+
+const fiveCast = () => {
+  const txt = $('#searchBar').val();
+  return new Promise((resolve, reject) => {
+    $.ajax(`http://api.openweathermap.org/data/2.5/forecast?zip=${txt},us&units=imperial&APPID=${weatherKey}`)
+      .done((result) => {
+        resolve(result);
+        console.log(result);
+      }).fail((err) => {
+        reject(err);
+      });
+  });
+};
+
+const showFiveCast = () => {
+  fiveCast()
+    .then((result) => {
+      dom.domStringFive(result);
+    })
+    .catch((err) => {
+      console.error('search error', err);
+    });
 };
 
 module.exports = {
   showResults,
   setKey,
+  showFiveCast,
 };
